9387e7cd242d1bf355841b973b3fbe349433ddad
lptaszek-cl <lptaszek@corelogic.com>
2021-01-28 09:08:28 +0100

    change annotations for AuthenticationRequest

--- src/main/java/com/fitcrew/jwt/model/AuthenticationRequest.java
+++ src/main/java/com/fitcrew/jwt/model/AuthenticationRequest.java
@@ -1,25 +1,20 @@
 package com.fitcrew.jwt.model;
 
-import com.fitcrew.FitCrewAppConstant.message.ValidationErrorMessage;
 import com.fitcrew.FitCrewAppConstant.message.type.RoleType;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
-@Data
+@Getter
+@Setter
+@Builder(toBuilder = true)
+@AllArgsConstructor(onConstructor = @__(@Builder))
 @NoArgsConstructor
-@AllArgsConstructor
+@ToString
 public class AuthenticationRequest implements Serializable {
 
-    @NotNull(message = ValidationErrorMessage.DATE_OF_BIRTH_ERROR_MESSAGE)
+    private static final long serialVersionUID = 2118331620631970477L;
     private String email;
-
-    @NotNull(message = ValidationErrorMessage.PASSWORD_ERROR_MESSAGE)
     private String password;
-
-    @NotNull(message = ValidationErrorMessage.ROLE_ERROR_MESSAGE)
     private RoleType role;
 }
